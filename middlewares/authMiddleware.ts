6c74d454c8e75b98e6553f2d3f17605897e5add7
Jose Cardona <jpcardonamena@gmail.com>
2024-12-19 17:29:18 +0100

    debbuging v2

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { UserRole } from "../enums/userRole";
-import {error} from "winston";
 
 export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
@@ -20,22 +19,16 @@ export const authenticateJWT = (req: Request, res: Response, next: NextFunction)
             roles: string[]
         };
 
-        console.log("Decoded JWT:", decoded);
-
         // Validate roles to ensure they match the UserRole enum
         const validatedRoles = decoded.roles.filter((role) =>
             Object.values(UserRole).includes(role as UserRole)
         ) as UserRole[];
-
-        console.error("Token verification failed:", error);
 
         req.user = {
             id: decoded.id,
             email: decoded.email,
             roles: validatedRoles, // Only valid UserRole values
         };
-
-        console.log("req.user:", req.user);
 
         next();
     } catch (error) {
